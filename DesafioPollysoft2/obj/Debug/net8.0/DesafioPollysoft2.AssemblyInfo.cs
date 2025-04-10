2028f605097756f416d187deeee29dd0a2cb5518
AndreyCavalcante <andrey.cavalcante06@gmail.com>
2025-04-10 15:23:20 -0300

    Ajustes finais

--- DesafioPollysoft2/obj/Debug/net8.0/DesafioPollysoft2.AssemblyInfo.cs
+++ DesafioPollysoft2/obj/Debug/net8.0/DesafioPollysoft2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioPollysoft2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd807b03e115473d49f126e08a2b026af6421ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioPollysoft2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioPollysoft2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
